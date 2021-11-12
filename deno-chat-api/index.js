07968b2629054da814230562a8d6c5ecd9e455fa
chosangho <teha007@naver.com>
2021-11-13 01:02:24 +0900

    first commit

---
+++ deno-chat-api/index.js
@@ -0,0 +1,30 @@
+import { Application, Router } from 'https://deno.land/x/oak@v7.7.0/mod.ts'
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts'
+
+const messages = []
+const channel = new BroadcastChannel('chat')
+channel.onmessage = (event) => {
+  messages.push(event.data)
+}
+
+const router = new Router()
+router
+  .get('/', (context) => {
+    context.response.body = 'Chat server!'
+  })
+  .get('/messages', (context) => {
+    context.response.body = messages
+  })
+  .post('/messages', async (context) => {
+    const message = await context.request.body().value
+    messages.push(message)
+    channel.postMessage(message)
+    context.response.body = messages
+  })
+
+const app = new Application()
+app.use(oakCors())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+addEventListener('fetch', app.fetchEventHandler())
